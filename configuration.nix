bd37d0ccb6d8d64e09326f8ae52daf444268e637
christan <ccavrey@gmail.com>
2024-04-24 19:13:32 +0000

    Adding WSL docker-desktop support

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,6 @@
 
   wsl.enable = true;
   wsl.defaultUser = "christan";
-  wsl.docker-desktop.enable = true;
   wsl.wslConf.network.hostname = "chrisdevops";
   wsl.wslConf.network.generateResolvConf = false;
   wsl.wslConf.boot.command = ""; #Default startup commands
@@ -46,6 +45,27 @@
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "terraform"
   ];
+
+  #Docker-desktop workaround to work with WSL
+  #Enable WSL integration on docker desktop
+  #https://github.com/nix-community/NixOS-WSL/issues/235
+  wsl.docker-desktop.enable = false;
+  wsl.extraBin = with pkgs; [
+      # Binaries for Docker Desktop wsl-distro-proxy
+      { src = "${coreutils}/bin/mkdir"; }
+      { src = "${coreutils}/bin/cat"; }
+      { src = "${coreutils}/bin/whoami"; }
+      { src = "${coreutils}/bin/ls"; }
+      { src = "${busybox}/bin/addgroup"; }
+      { src = "${su}/bin/groupadd"; }
+      { src = "${su}/bin/usermod"; }
+  ];
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+  systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
