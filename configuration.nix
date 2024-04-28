e5cecca0ac9f9b369fd0c24735510233a7997884
christan <ccavrey@gmail.com>
2024-04-28 09:42:17 +0000

    Adding rootless docker

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,10 @@
     enableOnBoot = true;
     autoPrune.enable = true;
   };
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
   systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 
   # This value determines the NixOS release from which the default
